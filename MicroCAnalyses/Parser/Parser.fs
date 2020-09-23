57dc312a133256077adb7d44ce44eed0cb2b085a
Sarah A. M. van Dam <s153659@student.dtu.dk>
2020-09-23 18:39:48 +0200

    Added type annotation for parser

--- MicroCAnalyses/Parser/Parser.fs
+++ MicroCAnalyses/Parser/Parser.fs
@@ -1,6 +1,6 @@
 module Parser
 open FSharp.Text.Lexing
-let parse program = 
+let parse program : MicroCTypes.expr = 
         let lexbuf = LexBuffer<char>.FromString program
         let res = MicroCParser.start MicroCLexer.tokenize lexbuf
         res
