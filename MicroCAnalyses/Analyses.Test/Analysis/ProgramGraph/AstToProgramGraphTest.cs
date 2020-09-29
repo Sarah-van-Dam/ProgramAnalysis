642b9251c37d72272d0b897cf2d636a0fcdad0b5
Andreas <bloxxer9001@gmail.com>
2020-09-29 12:43:38 +0200

    Changed program for TestSimpleGraph and TestGenerateSumIntegerGraph to be valid, and fixed tests to check more throughout on node naming conflicts and be consistent with the config node prefix/start/end.

--- MicroCAnalyses/Analyses.Test/Analysis/ProgramGraph/AstToProgramGraphTest.cs
+++ MicroCAnalyses/Analyses.Test/Analysis/ProgramGraph/AstToProgramGraphTest.cs
@@ -18,11 +18,11 @@ namespace Analyses.Test.Analysis.ProgramGraph
         {
             Graph.ProgramGraph graph = new Graph.ProgramGraph(
                 Parser.parse(
-                    "int x;"
+                    "int x; x := 2;"
                 )
             );
 
-            Assert.True(graph.Nodes.Count == 2, "The number of nodes in the graph should be 2, got " + graph.Nodes.Count);
+            Assert.True(graph.Nodes.Count == 3, "The number of nodes in the graph should be 3, got " + graph.Nodes.Count);
             Assert.True(graph.Edges.Any(edge => edge.Action is IntDeclaration), "Expected an edge with a variable declaration, but none was found.");
         }
 
@@ -35,10 +35,16 @@ namespace Analyses.Test.Analysis.ProgramGraph
                 )
             );
 
-            Assert.True(graph.Nodes.Count == 16, "The number of nodes in the graph should be 16, got " + graph.Nodes.Count);
-            Assert.True(graph.Nodes.Any(node => node.Name == "Q14"), "Expected Node 'Q14' was not found.");
-            Assert.True(graph.Nodes.Any(node => node.Name == "Q_End"), "Expected Node 'Q_End' was not found.");
-            Assert.True(graph.Edges.Count == 17, "The number of edges in the graph should be 17, got " + graph.Edges.Count);
+            Assert.True(graph.Nodes.Count == 15, "The number of nodes in the graph should be 15, got " + graph.Nodes.Count);
+            for (int i = 1; i <= 13; i++)
+            {
+                Assert.True(
+                    graph.Nodes.Any(node => node.Name == Graph.ProgramGraph.NodePrefix + i),
+                    $"Expected Node '{Graph.ProgramGraph.NodePrefix + i}' was not found."
+                );
+            }
+            Assert.True(graph.Nodes.Any(node => node.Name == Graph.ProgramGraph.EndNode), $"Expected Node '{Graph.ProgramGraph.EndNode}' was not found.");
+            Assert.True(graph.Edges.Count == 16, "The number of edges in the graph should be 16, got " + graph.Edges.Count);
             Assert.True(graph.Edges.Any(edge => edge.Action is Condition), "Expected an edge with a condition, but none was found.");
         }
 
@@ -47,13 +53,19 @@ namespace Analyses.Test.Analysis.ProgramGraph
         {
             Graph.ProgramGraph graph = new Graph.ProgramGraph(
                 Parser.parse(
-                    "int n[6]; int x; int r; n[0] := 2; n[1] := 7; n[2] := 1; n[3] := 9; n[4] := 2; n[5] := 5; x := 0; r := 0; while (x < 6) { r := r + n[x]; x := x + 1; }"
+                    "int[6] n; int x; int r; n[0] := 2; n[1] := 7; n[2] := 1; n[3] := 9; n[4] := 2; n[5] := 5; x := 0; r := 0; while (x < 6) { r := r + n[x]; x := x + 1; }"
                 )
             );
 
-            Assert.True(graph.Nodes.Count == 16, "The number of nodes in the graph should be 16, got " + graph.Nodes.Count);
-            Assert.True(graph.Nodes.Any(node => node.Name == "Q9"), "Expected Node 'Q9' was not found.");
-            Assert.True(graph.Nodes.Any(node => node.Name == "Q_End"), "Expected Node 'Q_End' was not found.");
+            Assert.True(graph.Nodes.Count == 15, "The number of nodes in the graph should be 15, got " + graph.Nodes.Count);
+            for (int i = 1; i <= 13; i++)
+            {
+                Assert.True(
+                    graph.Nodes.Any(node => node.Name == Graph.ProgramGraph.NodePrefix + i),
+                    $"Expected Node '{Graph.ProgramGraph.NodePrefix + i}' was not found."
+                );
+            }
+            Assert.True(graph.Nodes.Any(node => node.Name == Graph.ProgramGraph.EndNode), $"Expected Node '{Graph.ProgramGraph.EndNode}' was not found.");
             Assert.True(graph.Edges.Count == 15, "The number of edges in the graph should be 15, got " + graph.Edges.Count);
             Assert.True(graph.Edges.Any(edge => edge.Action is ArrayAssignment), "Expected an edge with an array assignment, but none was found.");
         }
