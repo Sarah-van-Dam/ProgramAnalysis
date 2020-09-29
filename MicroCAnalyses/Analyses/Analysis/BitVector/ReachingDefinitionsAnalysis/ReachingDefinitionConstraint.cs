fb6d1f8dc5f9a9dfe932242e7f3c839ab03a9670
Casper Skjærris <casper_skj@live.dk>
2020-09-29 14:34:20 +0200

    Renamed tuple field

--- MicroCAnalyses/Analyses/Analysis/BitVector/ReachingDefinitionsAnalysis/ReachingDefinitionConstraint.cs
+++ MicroCAnalyses/Analyses/Analysis/BitVector/ReachingDefinitionsAnalysis/ReachingDefinitionConstraint.cs
@@ -1,18 +1,16 @@
 using System.Collections.Generic;
-using Analyses.Graph;
+using System.Linq;
 
 namespace Analyses.Analysis.BitVector.ReachingDefinitionsAnalysis
 {
     public class ReachingDefinitionConstraints : IConstraints
     {
-        public readonly Dictionary<string, HashSet<(string action, string startNode, string endNode)>>
+        public readonly Dictionary<string, HashSet<(string variable, string startNode, string endNode)>>
             VariableToPossibleAssignments;
 
         public ReachingDefinitionConstraints()
         {
-           VariableToPossibleAssignments = new Dictionary<string, HashSet<(string action, string startNode, string endNode)>>();
+           VariableToPossibleAssignments = new Dictionary<string, HashSet<(string variable, string startNode, string endNode)>>();
         }
-        
-
     }
 }
