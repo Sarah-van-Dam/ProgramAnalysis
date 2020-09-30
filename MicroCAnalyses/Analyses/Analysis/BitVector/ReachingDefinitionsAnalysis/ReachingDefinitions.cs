8e976a2251969e8be9ff7d81e966e9bbae52022f
mtacheva <mariya.tacheva@gmx.com>
2020-09-30 16:36:18 +0200

    temp hack - dont complain about missing Cond

--- MicroCAnalyses/Analyses/Analysis/BitVector/ReachingDefinitionsAnalysis/ReachingDefinitions.cs
+++ MicroCAnalyses/Analyses/Analysis/BitVector/ReachingDefinitionsAnalysis/ReachingDefinitions.cs
@@ -78,8 +78,9 @@ namespace Analyses.Analysis.BitVector.ReachingDefinitionsAnalysis
                     // A write cannot kill anything
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(edge.Action), edge.Action,
-                        $"No kill set has been generated for this action: {edge.Action} ");
+                    break;
+                    //throw new ArgumentOutOfRangeException(nameof(edge.Action), edge.Action,
+                    //    $"No kill set has been generated for this action: {edge.Action} ");
             }
         }
 
@@ -130,8 +131,9 @@ namespace Analyses.Analysis.BitVector.ReachingDefinitionsAnalysis
                     // A write cannot generate anything
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException(nameof(edge.Action), edge.Action,
-                        $"No gen set has been generated for this action: {edge.Action} ");
+                    break;
+                    //throw new ArgumentOutOfRangeException(nameof(edge.Action), edge.Action,
+                    //    $"No gen set has been generated for this action: {edge.Action} ");
             }
         }
     }
