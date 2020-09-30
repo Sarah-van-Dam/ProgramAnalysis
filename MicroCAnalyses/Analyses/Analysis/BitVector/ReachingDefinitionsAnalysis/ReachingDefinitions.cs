cb2326a71e07746b11d0044613a392630366a8d0
Casper Skjærris <casper_skj@live.dk>
2020-09-30 18:43:54 +0200

    Made kill and gen sets for array declaration

--- MicroCAnalyses/Analyses/Analysis/BitVector/ReachingDefinitionsAnalysis/ReachingDefinitions.cs
+++ MicroCAnalyses/Analyses/Analysis/BitVector/ReachingDefinitionsAnalysis/ReachingDefinitions.cs
@@ -5,12 +5,13 @@ using Analyses.Analysis.Actions;
 using Analyses.Graph;
 
 [assembly: InternalsVisibleTo("Analyses.Test")]
+
 namespace Analyses.Analysis.BitVector.ReachingDefinitionsAnalysis
 {
     public class ReachingDefinitions : BitVectorFramework
     {
         private readonly ReachingDefinitionConstraints _constraints;
-        
+
         internal ReachingDefinitionConstraints Constraints => _constraints;
 
         public ReachingDefinitions(ProgramGraph programGraph)
@@ -26,13 +27,12 @@ namespace Analyses.Analysis.BitVector.ReachingDefinitionsAnalysis
         {
             foreach (var variableName in _program.VariableNames)
             {
-                _constraints.VariableToPossibleAssignments[variableName] = 
+                _constraints.VariableToPossibleAssignments[variableName] =
                     new HashSet<(string variable, string startNode, string endNode)>
                     {
                         (variableName, "?", ProgramGraph.StartNode)
                     };
             }
-
         }
 
         public override void Kill(Edge edge)
@@ -43,36 +43,40 @@ namespace Analyses.Analysis.BitVector.ReachingDefinitionsAnalysis
                     // A declaration cannot kill anything
                     break;
                 case ArrayDeclaration arrayDeclaration:
-                    // A declaration cannot kill anything
+                    _constraints.VariableToPossibleAssignments[arrayDeclaration.ArrayName] =
+                        new HashSet<(string variable, string startNode, string endNode)>();
                     break;
                 case RecordDeclaration recordDeclaration:
                     // A declaration cannot kill anything
                     break;
                 case IntAssignment intAssignment:
-                    _constraints.VariableToPossibleAssignments[intAssignment.VariableName] = 
+                    _constraints.VariableToPossibleAssignments[intAssignment.VariableName] =
                         new HashSet<(string variable, string startNode, string endNode)>();
                     break;
                 case ArrayAssignment arrayAssignment:
                     // An array assignment cannot kill anything because of amalgamation
                     break;
                 case RecordMemberAssignment recordMemberAssignment:
-                    _constraints.VariableToPossibleAssignments[$"{recordMemberAssignment.RecordName}.{recordMemberAssignment.RecordMember}"] = 
+                    _constraints.VariableToPossibleAssignments[
+                            $"{recordMemberAssignment.RecordName}.{recordMemberAssignment.RecordMember}"] =
                         new HashSet<(string variable, string startNode, string endNode)>();
                     break;
                 case RecordAssignment recordAssignment:
-                    _constraints.VariableToPossibleAssignments[$"{recordAssignment.RecordName}.{RecordMember.Fst}"] = 
+                    _constraints.VariableToPossibleAssignments[$"{recordAssignment.RecordName}.{RecordMember.Fst}"] =
                         new HashSet<(string variable, string startNode, string endNode)>();
                     _constraints.VariableToPossibleAssignments[$"{recordAssignment.RecordName}.{RecordMember.Snd}"] =
                         new HashSet<(string variable, string startNode, string endNode)>();
                     break;
                 case ReadVariable read:
-                    _constraints.VariableToPossibleAssignments[read.VariableName] = new HashSet<(string variable, string startNode, string endNode)>();
+                    _constraints.VariableToPossibleAssignments[read.VariableName] =
+                        new HashSet<(string variable, string startNode, string endNode)>();
                     break;
                 case ReadArray readArray:
                     // A read from an array cannot kill anything because of amalgamation
                     break;
                 case ReadRecordMember recordMember:
-                    _constraints.VariableToPossibleAssignments[$"{recordMember.RecordName}.{recordMember.RecordMember}"] = 
+                    _constraints.VariableToPossibleAssignments[$"{recordMember.RecordName}.{recordMember.RecordMember}"]
+                        =
                         new HashSet<(string variable, string startNode, string endNode)>();
                     break;
                 case Write write:
@@ -92,7 +96,8 @@ namespace Analyses.Analysis.BitVector.ReachingDefinitionsAnalysis
                     // A declaration cannot generate anything
                     break;
                 case ArrayDeclaration arrayDeclaration:
-                    // A declaration cannot generate anything
+                    _constraints.VariableToPossibleAssignments[arrayDeclaration.ArrayName].Add((
+                        arrayDeclaration.ArrayName, edge.FromNode.Name, edge.ToNode.Name));
                     break;
                 case RecordDeclaration recordDeclaration:
                     // A declaration cannot generate anything
@@ -106,14 +111,19 @@ namespace Analyses.Analysis.BitVector.ReachingDefinitionsAnalysis
                         .Add((arrayAssignment.ArrayName, edge.FromNode.Name, edge.ToNode.Name));
                     break;
                 case RecordMemberAssignment recordMemberAssignment:
-                    _constraints.VariableToPossibleAssignments[$"{recordMemberAssignment.RecordName}.{recordMemberAssignment.RecordMember}"]
-                        .Add(($"{recordMemberAssignment.RecordName}.{recordMemberAssignment.RecordMember}", edge.FromNode.Name, edge.ToNode.Name));
+                    _constraints
+                        .VariableToPossibleAssignments[
+                            $"{recordMemberAssignment.RecordName}.{recordMemberAssignment.RecordMember}"]
+                        .Add(($"{recordMemberAssignment.RecordName}.{recordMemberAssignment.RecordMember}",
+                            edge.FromNode.Name, edge.ToNode.Name));
                     break;
                 case RecordAssignment recordAssignment:
                     _constraints.VariableToPossibleAssignments[$"{recordAssignment.RecordName}.{RecordMember.Fst}"]
-                        .Add(($"{recordAssignment.RecordName}.{RecordMember.Fst}", edge.FromNode.Name, edge.ToNode.Name));
+                        .Add(($"{recordAssignment.RecordName}.{RecordMember.Fst}", edge.FromNode.Name,
+                            edge.ToNode.Name));
                     _constraints.VariableToPossibleAssignments[$"{recordAssignment.RecordName}.{RecordMember.Snd}"]
-                        .Add(($"{recordAssignment.RecordName}.{RecordMember.Snd}", edge.FromNode.Name, edge.ToNode.Name));
+                        .Add(($"{recordAssignment.RecordName}.{RecordMember.Snd}", edge.FromNode.Name,
+                            edge.ToNode.Name));
                     break;
                 case ReadVariable readVariable:
                     _constraints.VariableToPossibleAssignments[readVariable.VariableName]
@@ -125,7 +135,8 @@ namespace Analyses.Analysis.BitVector.ReachingDefinitionsAnalysis
                     break;
                 case ReadRecordMember recordMember:
                     _constraints.VariableToPossibleAssignments[$"{recordMember.RecordName}.{recordMember.RecordMember}"]
-                        .Add(($"{recordMember.RecordName}.{recordMember.RecordMember}", edge.FromNode.Name, edge.ToNode.Name));                    
+                        .Add(($"{recordMember.RecordName}.{recordMember.RecordMember}", edge.FromNode.Name,
+                            edge.ToNode.Name));
                     break;
                 case Write write:
                     // A write cannot generate anything
