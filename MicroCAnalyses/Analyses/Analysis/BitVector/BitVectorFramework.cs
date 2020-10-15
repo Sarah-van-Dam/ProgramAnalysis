b50ec18fad1bdeb7bddcb08f5309523abb65923b
Casper Skjærris <casper_skj@live.dk>
2020-10-15 17:00:38 +0200

    cleaned usings

--- MicroCAnalyses/Analyses/Analysis/BitVector/BitVectorFramework.cs
+++ MicroCAnalyses/Analyses/Analysis/BitVector/BitVectorFramework.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using Analyses.Graph;
-using Microsoft.VisualBasic;
 using Action = Analyses.Analysis.Actions;
 
 [assembly: InternalsVisibleTo("Analyses.Test")]
