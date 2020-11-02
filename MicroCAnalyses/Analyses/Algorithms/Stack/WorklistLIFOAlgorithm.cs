d615d70080607cd2eead024bbf4e5697e82d30d9
Andreas <bloxxer9001@gmail.com>
2020-11-02 16:02:38 +0100

    Implement Worklist Algo LIFO class. Missing test.

---
+++ MicroCAnalyses/Analyses/Algorithms/Stack/WorklistLIFOAlgorithm.cs
@@ -0,0 +1,28 @@
+﻿using Analyses.Graph;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Analyses.Algorithms.Stack
+{
+	class WorklistLIFOAlgorithm : WorklistAlgorithm
+	{
+		private Stack<Node> stack;
+
+		public override bool Empty()
+			=> stack.Count == 0;
+
+		public override Node Extract()
+			=> stack.Pop();
+
+		public override void Insert(Node q)
+			=> stack.Push(q);
+
+		/*
+		 * TO-DO:
+		 * Since FIFO and LIFO are deterministic,
+		 * create a counter for it with a test
+		 * to check that the counter never changes.
+		 */
+	}
+}
